1cb45bd04e810e022e8d3f6fbbd7b101c1b8f485
sani-Alpha <rai.sanidhya2010@gmail.com>
2021-09-21 17:01:02 +0530

    fixed init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void run() => runApp(MaterialApp(home: App()));
+void main() => runApp(MaterialApp(home: App()));
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
